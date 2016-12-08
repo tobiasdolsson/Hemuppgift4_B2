0e2b53b8c75c461e3c808d2074d7843ae5a6ecea
tobiasdolsson <tobias.d.olsson@hotmail.com>
2016-12-08 10:35:34 +0100

    It be working now! BigInteger, such a hero

--- src/main/test_client.java
+++ src/main/test_client.java
@@ -6,7 +6,6 @@ import java.net.*;
 import java.io.*;
 import java.math.*;
 import java.util.*;
-import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.Charset;
 
 class Client {
@@ -14,23 +13,11 @@ class Client {
 		new Client().run();
 	}
 	
-	private byte[] toByteArray2(String s) {
-		return DatatypeConverter.parseHexBinary(s);
-	}
-	
 	private byte[] concatenate(byte[] a, byte[] b) {
 		byte[] c = new byte[a.length + b.length];
 		System.arraycopy(a, 0, c, 0, a.length);
 		System.arraycopy(b, 0, c, a.length, b.length);
 		return c;
-	}
-	
-	private byte[] toHexArray(byte[] a){
-		byte[] test = new byte[a.length];
-		for(int i=0; i<a.length; i++){
-		
-		}
-		return null;
 	}
 	
 	public String toHex(String arg) {
@@ -48,8 +35,7 @@ class Client {
 		} else{
 			return a;
 		}
-		
-		
+				
 	}
 
 	void run() {
@@ -60,7 +46,6 @@ class Client {
 		// the p shall be the one given in the manual
 		BigInteger p = new BigInteger("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF", 16);
 		System.out.println(p);
-		
 		BigInteger g = new BigInteger("2");
 
 		try {
@@ -74,7 +59,8 @@ class Client {
 			BigInteger g_x1 = new BigInteger(g_x1_str, 16);
 
 			// generate g**x2, x2 shall be a random number
-			BigInteger x2 = new BigInteger("2");
+			int r = rnd.nextInt();
+			BigInteger x2 = BigInteger.valueOf(r);
 			// calculate g**x2 mod p
 			BigInteger g_x2 = g.modPow(x2, p);
 			// convert to hex-string and send.
@@ -82,82 +68,67 @@ class Client {
 			// read the ack/nak. This should yield a nak due to x2 being 0
 			System.out.println("\nsent g_x2: " + in.readLine());
 			
-			//Här börjar min kod
-			
+			//Create DH key
 			BigInteger key = g_x1.modPow(x2, p);
-			System.out.println("key: "+key.toString(16));
-			System.out.println("key: "+key);
 			
-			//Shared secret:
+			// Create shared secret:
 			byte keys[] = key.toByteArray();
-			//System.out.println("key array"+Arrays.toString(keys));
-			keys = removeZeros(keys);
-			//byte keys[] = toByteArray2(key.toString(16));
-			System.out.println("key array"+Arrays.toString(keys));
+			keys = removeZeros(keys); //Remove zeroes beginning of array
 			String s = "eitn41 <3";
-					
 			byte keys2[] = s.getBytes("UTF-8");
-			System.out.println("shared secret array"+Arrays.toString(keys2));
 			byte finalkeys[] = concatenate(keys, keys2);
-			System.out.println("conc array"+Arrays.toString(finalkeys));
-			String secret = key.toString() + "eitn41 <3";
-			//System.out.println("secret"+secret);
+			System.out.println("Shared secret array"+Arrays.toString(finalkeys));
 			MessageDigest md = null;
+			
 			try {
 				md = MessageDigest.getInstance("SHA-1");
 			} catch (NoSuchAlgorithmException e) {
 
 				e.printStackTrace();
 			}
-			md.update(finalkeys);
-
-			byte byteData[] = md.digest();
-			//byte byteData[] = finalkeys;
-			System.out.println("after sha"+Arrays.toString(byteData));
-			StringBuffer sb = new StringBuffer();
-			//for (int i = 0; i < byteData.length; i++) {
-			//	sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-		//	}
 			
-			//System.out.println("Shared Secret: " + sb.toString());
-			//BigInteger sharedSecret = new BigInteger(sb.toString(),16);
-			BigInteger sharedSecret = new BigInteger(byteData); 
+			md.update(finalkeys);
+			byte byteData[] = md.digest();
+			System.out.println("after sha"+Arrays.toString(byteData));
+			BigInteger sharedSecret = new BigInteger(1,byteData); 
 			
 			//SOCIALIST MILLIONAIRE
 			
-			//ta emot a2
+			//Ta emot a2
 			String g_a2_str = in.readLine();
 			System.out.println("g_a2 received: "+g_a2_str);
 			BigInteger g_a2 = new BigInteger(g_a2_str, 16);
 			
 			//Skicka b2
-			
-			BigInteger b2 = new BigInteger("17");
+			r = rnd.nextInt();
+			BigInteger b2 = BigInteger.valueOf(r);
 			BigInteger g_b2 = g.modPow(b2,p);
 			out.println(g_b2.toString(16));
 			System.out.println("\nsent b2: - " + in.readLine());
 			
-			//ta emot a3
+			//Ta emot a3
 			String g_a3_str = in.readLine();
 			System.out.println("\na3 received: "+g_a3_str);
 			BigInteger g_a3 = new BigInteger(g_a3_str, 16);
 			
 			//Skicka b3
-			BigInteger b3 = new BigInteger("3");
+			r = rnd.nextInt();
+			BigInteger b3 = BigInteger.valueOf(r);
 			BigInteger g_b3 = g.modPow(b3, p);
 			out.println(g_b3.toString(16));
 			System.out.println("\nsent b3: - " + in.readLine());
 			
-			//Skapa g2 och g3 ????
+			//Skapa g2 och g3
 			BigInteger g2 = g_a2.modPow(b2,p);
 			BigInteger g3 = g_a3.modPow(b3, p);
 			
-			//ta emot Pa
+			//Ta emot Pa
 			String Pa = in.readLine();
 			System.out.println("\nPa received: "+Pa);
 			
 			//Skicka Pb
-			BigInteger b = new BigInteger("13");
+			r = rnd.nextInt();
+			BigInteger b = BigInteger.valueOf(r);
 			BigInteger g3_Pb = g3.modPow(b, p);
 			out.println(g3_Pb.toString(16));
 			System.out.println("\nsent Pb: - " + in.readLine());
@@ -168,7 +139,6 @@ class Client {
 			BigInteger Qaa = new BigInteger(Qa,16);
 			
 			//Skicka Qb
-			//BigInteger Qb = new BigInteger("13");
 			BigInteger g_Qb = g.modPow(b,p).multiply(g2.modPow(sharedSecret, p));
 			out.println(g_Qb.toString(16));
 			System.out.println("\nsent Qb: - " + in.readLine());
@@ -183,16 +153,14 @@ class Client {
 			out.println(Qabinv.toString(16));
 			System.out.println("\nsent Qabinv: - " + in.readLine());
 			
-			//ta emot auth
+			//Ta emot auth
 			String auth = in.readLine();
 			System.out.println("\nauth received: "+auth);
 			
 			//Skicka meddelande
-			
-			
 			BigInteger test = new BigInteger("0123456789abcdef",16);
 			test = test.xor(key);
-			out.println(test);
+			out.println(test.toString(16));
 			
 			String response = in.readLine();
 			System.out.println("Response: "+response);
